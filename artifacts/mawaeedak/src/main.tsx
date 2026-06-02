013d7ac9bc67dd3a2210f2f3e494e3194daecf75
DANGERMANS <hrq@hotmail.com>
2026-06-02 07:05:37 +0300

    Remove neutralized visual reference import

--- artifacts/mawaeedak/src/main.tsx
+++ artifacts/mawaeedak/src/main.tsx
@@ -1,7 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import "./styles/mawaeedak-reference.css";
 import { registerApiAuth } from "./lib/apiAuth";
 
 registerApiAuth();
